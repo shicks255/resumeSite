f70e0f3b40b50eba0cf91acca53110439e0932d9
shicks <shicks255@yahoo.com>
2018-01-04 10:05:29 -0500

    binary tree animal game almost done

--- src/main/java/com/steven/hicks/TechHandling/logics/BinaryTreeGameLogic.java
+++ src/main/java/com/steven/hicks/TechHandling/logics/BinaryTreeGameLogic.java
@@ -1,16 +1,19 @@
 package com.steven.hicks.TechHandling.logics;
 
 import com.steven.hicks.entities.BTNode;
+import com.steven.hicks.filters.LogFilter;
+import org.apache.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class BinaryTreeGameLogic
 {
+    private static final Logger log = Logger.getLogger(BinaryTreeGameLogic.class.getName());
+
     public static BTNode<String> getNodesFromXML()
     {
         InputStream inputStream =
@@ -78,6 +81,7 @@ public class BinaryTreeGameLogic
         catch (IOException e)
         {
             e.printStackTrace();
+            log.error(e.getMessage());
         }
 
         return root;
@@ -85,7 +89,65 @@ public class BinaryTreeGameLogic
 
     public static void addNode(String animal, String question, BTNode currentNode)
     {
+        BTNode<String> oldAnswer = new BTNode<String>((String)currentNode.getData(), null, null);
 
+        currentNode.setData(question);
+        currentNode.setLeft(new BTNode<String>(animal, null, null));
+        currentNode.setRight(oldAnswer);
+
+        try
+        {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            File file = new File(classLoader.getResource("animalGuessingTree.xml").getFile());
+            FileWriter fr = new FileWriter(file);
+            BufferedWriter writer = new BufferedWriter(fr);
+
+            BTNode<String> root = getNodesFromXML();
+            writer.write("<?xml version=\"1.0\"?>");
+            writer.write("\r\n\t<questionTree xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"animalTreeSchema.xsd\">");
+            writer.write("\r\n\t\t<node id=\"1   \" parentId=\"1   \" data=\"" + root.getData() + "\">");
+
+            Set<Integer> usedIds = new HashSet<>();
+            int currentId = 1;
+            int currentParent = 1;
+            usedIds.add(currentId);
+
+            writeLeftAndRightNodes(writer, root, usedIds, currentId, currentParent);
+
+            writer.write("\r\n\t\t</node>");
+            writer.write("\r\n\t</questionTree>");
+
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
     }
 
+    private static void writeLeftAndRightNodes(BufferedWriter writer, BTNode<String> currentNode, Set<Integer> usedIds, int currentId, int currentParent) throws IOException
+    {
+        BTNode<String> testNode = currentNode;
+        if (testNode.getLeft() != null)
+        {
+            testNode = testNode.getLeft();
+            while (!usedIds.add(currentId))
+                currentId++;
+
+            writer.write("\r\n\t\t\t<left id=\"" + currentId + "   \" parentId=\"" + currentParent + "   \" data=\"" + testNode.getData() + "\"/>");
+            writeLeftAndRightNodes(writer, testNode, usedIds, currentId, currentId);
+        }
+
+        testNode = currentNode;
+        if (testNode.getRight() != null)
+        {
+            testNode = testNode.getRight();
+            while (!usedIds.add(currentId))
+                currentId++;
+
+            writer.write("\r\n\t\t\t<right id=\"" + currentId + "   \" parentId=\"" + currentParent + "   \" data=\"" + testNode.getData() + "\"/>");
+            writeLeftAndRightNodes(writer, testNode, usedIds, currentId, currentId);
+        }
+    }
 }
